3fff53d8f2243c6df378493f2e28368c02a09492
Bartlomiej Stepien <dladmin@dladmins-MacBook-Pro.local>
2019-02-28 20:57:51 +0100

    change routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :microposts
   resources :doctors
+  resources :microposts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'doctors#index'
+  root 'application#hello'
 
 end
